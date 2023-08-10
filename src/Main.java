1035a4784ee288b8fbd64e74158929a3ca47e54b
Devang <shinigamiryuk389@gmail.com>
2023-08-10 18:34:16 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,198 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//        #1 Take two input from user and give the sum of two numbers
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Please enter the first no : ");
+//        int num1 = input.nextInt();
+//
+//        System.out.println("Please enter the second no : ");
+//        int num2 = input.nextInt();
+//
+//        int sum = num1 +num2;
+//
+//        System.out.println("The sum of two numbers is : " + sum);
+
+
+        // #2 Print numbers from 1 to n
+
+//        System.out.println("Please enter the number : ");
+//
+//        Scanner in = new Scanner(System.in);
+//
+//        int n = in.nextInt();
+//
+//        for (int num = 1; num <= n ; num++) {
+//            System.out.println(num);
+//
+//        }
+
+        // # find greatest no
+
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("Please enter first no : ");
+//        int a = in.nextInt();
+//
+//        System.out.println("Please enter second no : ");
+//        int b = in.nextInt();
+//
+//        System.out.println("Please enter the third no : ");
+//        int c = in.nextInt();
+//
+//        int max = a;
+//
+//        if (b > max){
+//            max = b;
+//        }
+//
+//        if(c>max){
+//            max =c;
+//        }
+//        System.out.println(max);
+
+        // # Fibonacci numbers
+
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("Please enter the Fibonacci ranked no u want : ");
+//
+//        int a  = 0;
+//        int b =  1;
+//        int count=2;
+//        int n = in.nextInt();
+//
+//        while(count<=n){
+//            int temp = b;
+//            b=a+b;
+//            a=temp;
+//            count++;
+//
+//        }
+//
+//        System.out.println(b);
+
+        // # Count how many times digit 5 is present in any number
+
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("Please enter the number : ");
+//
+//        int num = in.nextInt();
+//        int count=0;
+//        while(num>0) {
+//            int rem = num % 10;
+//            if (rem == 5) {
+//                count++;
+//            }
+//            num = num / 10;
+//        }
+//            System.out.print("The total no of digit 5 occuring in this number is " + count);
+
+
+        // # Basic calculator
+
+
+//        Scanner in = new Scanner(System.in);
+//        while (true) {
+//            int result = 0;
+//            System.out.println("Please enter the first no : ");
+//            int num1 = in.nextInt();
+//            System.out.println("Please enter the second no : ");
+//            int num2 = in.nextInt();
+//            System.out.println("Please enter the the operand : ");
+//            String op = in.next();
+//            if (op.equals("+")) {
+//                result = num1 + num2;
+//            } else if (op.equals("-")) {
+//                result = num1 - num2;
+//            } else if (op.equals("*")) {
+//                result = num1 * num2;
+//            } else {
+//                result = num1 / num2;
+//            }
+//            System.out.println(result);
+//
+//            System.out.println("Do you want to continue ? (Yes/No)");
+//            String choice = in.next();
+//            if (choice.equals("No")) {
+//                break;
+//            } else if (!choice.equals("Yes")) {
+//                System.out.println("Invlaid Operation ");
+//
+//            }
+//        }
+
+        // SWITCH STATEMENTS
+        //  Instead of using lot of if else with can use switch statements
+
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Please enter the name of fruit ");
+//        String fruit = in.next();
+//
+//        switch (fruit){
+//            case "Mango":
+//                System.out.println("King of fruits ");
+//                break;
+//
+//            case "Apple ":
+//                System.out.println(" I dont like it ");
+//                break;
+//            case "Banana":
+//                System.out.println("Tasty");
+//                break;
+//            case "Orange":
+//                System.out.println("Orange");
+//        }
+
+
+        // add two numbers but with the help of calling a function / method
+
+//        sum();
+//    }
+//
+//
+//    static void sum(){
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("Input first no : ");
+//        int num1 = in.nextInt();
+//        System.out.println("Input second no : ");
+//        int num2 =in.nextInt();
+//
+//        int sum = num1+num2;
+//
+//        System.out.println("The value of sum is " + sum );
+//  }
+//
+//  }
+
+        // idk maybe how to initliaze the value from other function for some other function
+//        carry(60);
+//    }
+//    static void carry (int a) {
+//        System.out.println(a);
+//        }
+//}
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
